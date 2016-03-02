eed9b9a97807f36876f69a70c44fbe92cebf70a6
vera zeng <zengheng810@gmail.com>
2016-03-02 18:27:13 +0800

    add a unit test

--- UnitTestDemoTests/UnitTestDemoTests.m
+++ UnitTestDemoTests/UnitTestDemoTests.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "Answer.h"
+#import "Criterion.h"
 
 @interface UnitTestDemoTests : XCTestCase
 
@@ -24,16 +26,20 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testParseAnswer {
+    Answer *answer = [[Answer alloc] initWithQuestion:@"quest" answer:@"answer"];
+    
+    XCTAssertEqualObjects(answer.question, @"quest");
+    XCTAssertEqualObjects(answer.answerText, @"answer");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testIsAnswerMatched {
+    Answer *answer = [[Answer alloc] initWithQuestion:@"quest" answer:@"answer"];
+    Criterion *c = [[Criterion alloc] initWithAnswer:answer weight:eCriterionMustMatch];
+    
+    BOOL isMatched = [c isAnswerMatched:answer];
+    
+    XCTAssertTrue(isMatched);
 }
 
 @end
